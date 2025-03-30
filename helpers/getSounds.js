bd1c73dc68bc5e5d2bba6ebd58cb4971dc3efea8
seoulection <6445410+seoulection@users.noreply.github.com>
2025-03-30 13:52:51 -0700

    adjusts play chances

--- helpers/getSounds.js
+++ helpers/getSounds.js
@@ -5,22 +5,22 @@ const getRandomMelody = () => {
     case 0:
       return { bell: 'eee.mp3', count: 1, melody: 'rusheintro.mp3' }
     case 1:
-    case 2:
       return { ...defaultMelody, bell: 'explosion.mp3', melody: 'ac130.mp3' }
-    case 3:
+    case 2:
       return { ...defaultMelody, melody: 'spetz.mp3' }
+    case 3:
     case 4:
-    case 5:
       return {
         ...defaultMelody,
         bell: ['bing.wav', 'bong.wav'],
         melody: 'bingbong.wav',
       }
+    case 5:
     case 6:
-    case 7:
       return { ...defaultMelody, bell: 'haha.mp3', melody: 'clappingass.mp3' }
-    case 8:
+    case 7:
       return { ...defaultMelody, bell: 'lobell.mp3', melody: 'lomelody.mp3' }
+    case 8:
     case 9:
       return { ...defaultMelody, melody: 'creep1.wav' }
   }
